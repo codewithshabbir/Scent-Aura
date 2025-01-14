7b36d5c5c90a58e2b0aa10e41b9694e711ab8d28
YahyaAbha <ffpubglite9@gmail.com>
2025-01-14 19:34:05 +0500

    update all files

---
+++ data.js
@@ -0,0 +1,184 @@
+export const Products = [
+    // For Man Categoty.....
+  {
+    Name: "Catch 22",
+    imgUrl: "./assets/images/products/Catch22copy_6_11zon.jpeg",
+    FeatureImg: "./assets/images/products/catch_22_100ml.jpeg",
+    Category: "Man",
+    Price: 1790,
+    Sale_Price: 1650,
+    Qty: 34,
+    Description:
+      "Catch 22 is a chypre fruity fragrance for men. The top notes of this perfume are Bergamot, Pineapple, Apple and Black Currant, while the middle notes are Patchouli, Moroccan Jasmine, Birch and Rose. The base notes include Musk, Ambergris, Oak moss and Vanille. Catch 22 is one of our best-selling fragrances and it is sure to get you lots of compliments. ",
+  },
+  {
+    Name: "Dauntless",
+    imgUrl: "./assets/images/products/Dauntlesscopy_18_11zon.jpeg",
+    FeatureImg: "./assets/images/products/Dauntless.jpg",
+    Category: "Man",
+    Price: 1600,
+    Sale_Price: 1550,
+    Qty: 23,
+    Description:
+      "Dauntless is a sweet and warm amber woody fragrance exclusively for men. This perfume has a very beautiful blend of warm, citrusy and sweet accords. The notes are well-blended creating a beautiful harmony in the scent. The top notes of this fragrance are bergamot, apple, neroli and lemon which are quite spicy and citrusy while the middle notes are patchouli, rose and teak wood. The base notes have musk and vanilla.",
+  },
+  {
+    Name: "Inferno",
+    imgUrl: "./assets/images/products/inferno_29_11zon.jpeg",
+    FeatureImg: "./assets/images/products/inferno.jpg",
+    Category: "Man",
+    Price: 1390,
+    Sale_Price: 1250,
+    Qty: 43,
+    Description:
+      "Inferno is an amber woody fragrance for women and men. Its notes consist of Agarwood, Brazilian Rosewood, Sandalwood, Cardamom, Vanilla, Sichuan Pepper, Vetiver, Tonka Bean and Amber. This is a one-of-a-kind perfume which only a few have the nose to appreciate. This perfume brings to mind a mysterious and daring individual who is very successful; this person is unconcerned with the world yet is incredibly magnetic. ",
+  },
+  {
+    Name: "Near You",
+    imgUrl: "./assets/images/products/NearYou-50mlcopy.jpeg",
+    FeatureImg: "./assets/images/products/nearyou_49_11zon.jpeg",
+    Category: "Man",
+    Price: 1620,
+    Sale_Price: 1500,
+    Qty: 22,
+    Description:
+      "Near You is a woody aquatic fragrance for men. The top notes consist of cypress, bergamot, mandarin, yuzu, clary sage, verbena, coriander and tarragon. The heart notes include  saffron, nutmeg, water lily, geranium and cinnamon. The base notes are tobacco, amber, musk, cypriol, sandalwood and vetiver. Wearing this perfume is like bathing in calmness.",
+  },
+//   For Woman Category....
+{
+    Name: "Merry Me",
+    imgUrl: "./assets/images/products/merryme_47_11zon.jpeg",
+    FeatureImg: "./assets/images/products/merry_me_d55db43c-ab98-4623-8bb1-b74cec9503d5.png",
+    Category: "Women",
+    Price: 1620,
+    Sale_Price: 1600,
+    Qty: 12,
+    Description:
+      "Merry Me is a floral fruity fragrance for women. This perfume is a mixture of floral and fruity accords which make for a wonderful combination.The top notes of this fragrance are Grapefruit, Passionfruit, Pineapple, Big Strawberry and Tangerine, while the middle notes are Vanilla orchid, Peony, Red Berries, Lily-of-the-Valley and Jasmine. The base notes are Woody Notes, Musk and Oakmoss. An attractive feminine perfume, this one is pure joy.",
+  },
+  {
+    Name: "Lady Eve",
+    imgUrl: "./assets/images/products/LAdyeve_37_11zon.jpeg",
+    FeatureImg: "./assets/images/products/LADY_EVE_ea351503-3e3f-4ddc-9a7e-5da095ce0700.png",
+    Category: "Women",
+    Price: 1320,
+    Sale_Price: 1300,
+    Qty: 34,
+    Description:
+      "Lady Eve is a chypre fruity fragrance for women. The top notes of this perfume are Californian Gardenia, Peach and African Freesia Petals, while the middle notes are Damask Rose, Coriander and Jasmine. Its base notes include Natural Vanilla, Patchouli and Vetiver. This is a strong irresistible feminine fragrance.",
+  },
+  {
+    Name: "Velvet Smooth",
+    imgUrl: "./assets/images/products/VelvetSmooth.jpg",
+    FeatureImg: "./assets/images/products/velvetsmooth_83_11zon.jpeg",
+    Category: "Women",
+    Price: 1520,
+    Sale_Price: 1510,
+    Qty: 31,
+    Description:
+      "Velvet Smooth is a floral fragrance for women. It has a beautiful blend of accords like rose, floral, warm spicy, musky, powdery and woody These combination of accords makes it a quite unique fragrance. The composition of notes are well balanced and form a beautiful harmony which spreads a magical aura around the wearer. The top notes are floral and fresh notes while the middle notes are spicy and floral notes. The base notes are musk and rose. The performance is excellent during spring, fall and summer and moderately acceptable during winters, thus, you can wear it throughout the year.",
+  },
+  {
+    Name: "Inferno",
+    imgUrl: "./assets/images/products/inferno_29_11zon.jpeg",
+    FeatureImg: "./assets/images/products/inferno.jpg",
+    Category: "Women",
+    Price: 1390,
+    Sale_Price: 1250,
+    Qty: 43,
+    Description:
+      "Inferno is an amber woody fragrance for women and men. Its notes consist of Agarwood, Brazilian Rosewood, Sandalwood, Cardamom, Vanilla, Sichuan Pepper, Vetiver, Tonka Bean and Amber. This is a one-of-a-kind perfume which only a few have the nose to appreciate. This perfume brings to mind a mysterious and daring individual who is very successful; this person is unconcerned with the world yet is incredibly magnetic. ",
+  },
+  // Eastern
+  {
+    Name: "Iqbal",
+    imgUrl: "./assets/images/products/Iqbal.webp",
+    FeatureImg: "./assets/images/products/Iqbal-2.webp",
+    Category: "Eastern",
+    Price: 4300,
+    Sale_Price: 4290,
+    Qty: 43,
+    Description:
+      "Iqbal is an Oriental Woody fragrance. It is one of the most unique unisex fragrances. It is a mature scent that can be worn in all weathers and occasions but is preferably suitable for fall, winter, and evening events. The perfume opens with top notes of Sicilian Bergamot, Pink Pepper, and Davana, giving it a citrusy and spicy start. As the scent develops, further middle notes like Agarwood (Oud), White Amber, and Rosemary become prominent. On the dry-down, you smell base notes like Leather, Musk, and Haitian Vetiver, giving a stronger and woody impact.",
+  },
+  {
+    Name: "Amber Oud Intense",
+    imgUrl: "./assets/images/products/amberoud.webp",
+    FeatureImg: "./assets/images/products/Amberoud-2.webp",
+    Category: "Eastern",
+    Price: 3180,
+    Sale_Price: 2540,
+    Qty: 30,
+    Description:
+      "After the success of our Amber Oud fragrance, we have formulated a stronger, smoother, and a more lasting version. Introducing Amber Oud 2.0, an intensified interpretation of the classic amber spicy fragrance, designed for both men and women. This luxurious scent boasts a rich blend of warm spices, roses, woods, amber, and oud, among others. The top notes have a zesty trio of lemon, bergamot, and lime, while the middle notes are of fig, rose, ylang-ylang, and jasmine, adding a floral sweetness.As it settles, the fragrance highlights ambergris, rose, and oud, delivering a smoky, slightly spicy base. Amber Oud 2.0 lasts up to 12 hours with a moderate scent trail, making it perfect for fall and winter, with some versatility for spring. It's your go-to for memorable evenings and nightouts.",
+  },
+  {
+    Name: "Lost Symbol",
+    imgUrl: "./assets/images/products/lostsymbol.webp",
+    FeatureImg: "./assets/images/products/lostsymbol-2.webp",
+    Category: "Eastern",
+    Price: 1990,
+    Sale_Price: 1890,
+    Qty: 30,
+    Description:
+      "Lost Symbol is an amber floral fragrance for men and women. The main accords of this perfume are oud, rose, woody, amber, warm spicy and patchouli. The combination of accords is beautiful and well balanced.The top notes of the fragrance are labdanum while the middle notes are rose, patchouli and saffron. Moreover, the base notes are agarwood, sandalwood and cedar. This perfume is oriental and floral with an enchanting scent that creates a magical aura around the wearer. Lost Symbol perfume performs best in all seasons except summers. ",
+  },
+  {
+    Name: "Addiction",
+    imgUrl: "./assets/images/products/Addictioncopy.webp",
+    FeatureImg: "./assets/images/products/AddictionBox-2.webp",
+    Category: "Eastern",
+    Price: 2950,
+    Sale_Price: 2990,
+    Qty: 30,
+    Description:
+      "Addiction is an aromatic woody fragrance for men and women. The fragrance is a beautiful blend of notes that creates an appealing aura. The top notes are amber and green notes while the middle notes are resins, woody notes, and coffee.The base notes are agarwood oud and incense. A scent with an intense olfactory signature with its notes of oud wood and incense that overwhelms the senses. It is an extremely powerful unisex perfume and a must-have scent in any fraghead’s perfume collection. ",
+  },
+  // Wastern
+
+  {
+    Name: "Da Vinci Code",
+    imgUrl: "./assets/images/products/DaVanciCodecopy.webp",
+    FeatureImg: "./assets/images/products/DaVinciCode-2.webp",
+    Category: "WAstern",
+    Price: 1650,
+    Sale_Price: 1630,
+    Qty: 43,
+    Description:
+      "Da Vinci Code is an aromatic aquatic fragrance for men. Being full of wind and water, this scent depicts freedom. It smells fresh like a beach and has a masculine character.The top notes are Lime, Lemon, Bergamot, Jasmine, Orange and Neroli, while the middle notes are Sea notes, Calone, Freesia, Rosemary, Coriander, Nutmeg, Rose and Violet. The base notes include White musk, Amber, Cedar, Patchouli and Oakmoss. This perfume is representative of the mediterranean: the ocean breeze, citruses, light flowers and a few dry trees. ",
+  },
+  {
+    Name: "Catch 22",
+    imgUrl: "./assets/images/products/Catch22copy.webp",
+    FeatureImg: "./assets/images/products/catch_22_100ml-2.webp",
+    Category: "Wastern",
+    Price: 1790,
+    Sale_Price: 1780,
+    Qty: 43,
+    Description:
+      "Catch 22 is a chypre fruity fragrance for men. The top notes of this perfume are Bergamot, Pineapple, Apple and Black Currant, while the middle notes are Patchouli, Moroccan Jasmine, Birch and Rose. The base notes include Musk, Ambergris, Oak moss and Vanille. Catch 22 is one of our best-selling fragrances and it is sure to get you lots of compliments.",
+  },
+  {
+    Name: "Tipping Point",
+    imgUrl: "./assets/images/products/Tipping_Point.webp",
+    FeatureImg: "./assets/images/products/Tipping_Point-2.webp",
+    Category: "Wastern",
+    Price: 2190,
+    Sale_Price: 2150,
+    Qty: 43,
+    Description:
+      "Tipping Point is a woody aromatic fragrance for men. The primary accords of this perfume are citrus, woody, warm spicy and aromatic. The top notes of this perfume are Grapefruit, Mint, Lemon and Pink Pepper, while the middle notes are Ginger, Jasmine, Nutmeg and Iso E Super. Its base notes include Incense, Cedar, Vetiver, Sandalwood, Labdanum, Patchouli and White Musk. This is a classic men’s fragrance that will be great for any occasion at any time of the year.",
+  },
+  {
+    Name: "Smash My Head (EDP)",
+    imgUrl: "./assets/images/products/smash_my_head.webp",
+    FeatureImg: "./assets/images/products/SmashMyHead-2.webp",
+    Category: "Wastern",
+    Price: 2590,
+    Sale_Price: 2580,
+    Qty: 43,
+    Description:
+      "Smash My Head is an aromatic fougere fragrance for men. The top notes of this perfume include Bergamot, Calabrian and Pepper, while the  middle notes are Lavender, Sichuan pepper, Pink Pepper, Patchouli, Vetiver, Elemi and Geranium. The base notes are Cedar, Ambroxan and Labdanum. This fragrance is extremely fresh and noble but it simultaneously also has a rawness to it.",
+  },
+
+];
